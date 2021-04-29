c1508e4403ca32fdee93f37836b6a7ad4e06c036
Truong Phuong <truongphuongvt1107@gmail.com>
2021-04-29 10:34:10 +0700

    Add project files.

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbKeySize = new System.Windows.Forms.ComboBox();
+            this.btnGenerateKeys = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPrivateKeyBrowse = new System.Windows.Forms.Button();
+            this.txbPrivateKeyFilePath = new System.Windows.Forms.TextBox();
+            this.btnPublicKeyBrowse = new System.Windows.Forms.Button();
+            this.txbPulicKeyFilePath = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnSourceBrowse = new System.Windows.Forms.Button();
+            this.txbSourcePath = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbbKeySize);
+            this.groupBox1.Controls.Add(this.btnGenerateKeys);
+            this.groupBox1.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(168, 208);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Generate keys";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Key size";
+            // 
+            // cbbKeySize
+            // 
+            this.cbbKeySize.FormattingEnabled = true;
+            this.cbbKeySize.Location = new System.Drawing.Point(21, 73);
+            this.cbbKeySize.Name = "cbbKeySize";
+            this.cbbKeySize.Size = new System.Drawing.Size(121, 24);
+            this.cbbKeySize.TabIndex = 4;
+            // 
+            // btnGenerateKeys
+            // 
+            this.btnGenerateKeys.Location = new System.Drawing.Point(21, 130);
+            this.btnGenerateKeys.Name = "btnGenerateKeys";
+            this.btnGenerateKeys.Size = new System.Drawing.Size(121, 26);
+            this.btnGenerateKeys.TabIndex = 3;
+            this.btnGenerateKeys.Text = "Generate Keys";
+            this.btnGenerateKeys.UseVisualStyleBackColor = true;
+            this.btnGenerateKeys.Click += new System.EventHandler(this.btnGenerateKeys_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnPrivateKeyBrowse);
+            this.groupBox2.Controls.Add(this.txbPrivateKeyFilePath);
+            this.groupBox2.Controls.Add(this.btnPublicKeyBrowse);
+            this.groupBox2.Controls.Add(this.txbPulicKeyFilePath);
+            this.groupBox2.Controls.Add(this.btnDecrypt);
+            this.groupBox2.Controls.Add(this.btnEncrypt);
+            this.groupBox2.Controls.Add(this.btnSourceBrowse);
+            this.groupBox2.Controls.Add(this.txbSourcePath);
+            this.groupBox2.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Black;
+            this.groupBox2.Location = new System.Drawing.Point(201, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(486, 208);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "RSA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 16);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Private Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Public Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Source file";
+            // 
+            // btnPrivateKeyBrowse
+            // 
+            this.btnPrivateKeyBrowse.Enabled = false;
+            this.btnPrivateKeyBrowse.Location = new System.Drawing.Point(382, 113);
+            this.btnPrivateKeyBrowse.Name = "btnPrivateKeyBrowse";
+            this.btnPrivateKeyBrowse.Size = new System.Drawing.Size(75, 25);
+            this.btnPrivateKeyBrowse.TabIndex = 23;
+            this.btnPrivateKeyBrowse.Text = "Browse";
+            this.btnPrivateKeyBrowse.UseVisualStyleBackColor = true;
+            this.btnPrivateKeyBrowse.Click += new System.EventHandler(this.btnPrivateKeyBrowse_Click);
+            // 
+            // txbPrivateKeyFilePath
+            // 
+            this.txbPrivateKeyFilePath.Location = new System.Drawing.Point(104, 113);
+            this.txbPrivateKeyFilePath.Name = "txbPrivateKeyFilePath";
+            this.txbPrivateKeyFilePath.ReadOnly = true;
+            this.txbPrivateKeyFilePath.Size = new System.Drawing.Size(256, 26);
+            this.txbPrivateKeyFilePath.TabIndex = 22;
+            // 
+            // btnPublicKeyBrowse
+            // 
+            this.btnPublicKeyBrowse.Enabled = false;
+            this.btnPublicKeyBrowse.Location = new System.Drawing.Point(382, 73);
+            this.btnPublicKeyBrowse.Name = "btnPublicKeyBrowse";
+            this.btnPublicKeyBrowse.Size = new System.Drawing.Size(75, 25);
+            this.btnPublicKeyBrowse.TabIndex = 21;
+            this.btnPublicKeyBrowse.Text = "Browse";
+            this.btnPublicKeyBrowse.UseVisualStyleBackColor = true;
+            this.btnPublicKeyBrowse.Click += new System.EventHandler(this.btnPublicKeyBrowse_Click);
+            // 
+            // txbPulicKeyFilePath
+            // 
+            this.txbPulicKeyFilePath.Location = new System.Drawing.Point(104, 73);
+            this.txbPulicKeyFilePath.Name = "txbPulicKeyFilePath";
+            this.txbPulicKeyFilePath.ReadOnly = true;
+            this.txbPulicKeyFilePath.Size = new System.Drawing.Size(256, 26);
+            this.txbPulicKeyFilePath.TabIndex = 20;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Enabled = false;
+            this.btnDecrypt.Location = new System.Drawing.Point(243, 152);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(117, 26);
+            this.btnDecrypt.TabIndex = 19;
+            this.btnDecrypt.Text = "Decryption";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Enabled = false;
+            this.btnEncrypt.Location = new System.Drawing.Point(104, 152);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(117, 26);
+            this.btnEncrypt.TabIndex = 18;
+            this.btnEncrypt.Text = "Encryption";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnSourceBrowse
+            // 
+            this.btnSourceBrowse.Location = new System.Drawing.Point(382, 30);
+            this.btnSourceBrowse.Name = "btnSourceBrowse";
+            this.btnSourceBrowse.Size = new System.Drawing.Size(75, 25);
+            this.btnSourceBrowse.TabIndex = 17;
+            this.btnSourceBrowse.Text = "Browse";
+            this.btnSourceBrowse.UseVisualStyleBackColor = true;
+            this.btnSourceBrowse.Click += new System.EventHandler(this.btnSourceBrowse_Click);
+            // 
+            // txbSourcePath
+            // 
+            this.txbSourcePath.Location = new System.Drawing.Point(104, 31);
+            this.txbSourcePath.Name = "txbSourcePath";
+            this.txbSourcePath.ReadOnly = true;
+            this.txbSourcePath.Size = new System.Drawing.Size(256, 26);
+            this.txbSourcePath.TabIndex = 16;
+            this.txbSourcePath.TextChanged += new System.EventHandler(this.txbSourcePath_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 248);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RSA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbKeySize;
+        private System.Windows.Forms.Button btnGenerateKeys;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPrivateKeyBrowse;
+        private System.Windows.Forms.TextBox txbPrivateKeyFilePath;
+        private System.Windows.Forms.Button btnPublicKeyBrowse;
+        private System.Windows.Forms.TextBox txbPulicKeyFilePath;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnSourceBrowse;
+        private System.Windows.Forms.TextBox txbSourcePath;
+    }
+}
+
---
+++ RSA/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Xml;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        private int MinOfKeySize = 384;
+        private int MaxOfKeySise = 16384;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int keySize = MinOfKeySize;
+            do
+            {
+                cbbKeySize.Items.Add(keySize.ToString());
+                keySize += 8;
+            }
+            while (keySize <= MaxOfKeySise);
+
+            cbbKeySize.SelectedIndex = 0;
+
+        }
+
+        private void btnGenerateKeys_Click(object sender, EventArgs e)
+        {
+            int keySize = int.Parse(cbbKeySize.SelectedItem.ToString());
+
+            CspParameters cspParam = new CspParameters(1);
+            RSACryptoServiceProvider rsaCrypto = new RSACryptoServiceProvider(keySize, cspParam);
+
+            String xmlPublicKey = rsaCrypto.ToXmlString(false);
+            String xmlPrivateKey = rsaCrypto.ToXmlString(true);
+
+            // Save Keys
+            StreamWriter privateKeyWriter = null;
+            StreamWriter publicKeyWriter = null;
+
+            saveFileDialog1.Title = "Save Private Key";
+            saveFileDialog1.FileName = "PrivateKey.prk";
+
+            bool isPrivateKeySaved = false;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                privateKeyWriter = new StreamWriter(saveFileDialog1.FileName);
+                privateKeyWriter.Write(xmlPrivateKey);
+                privateKeyWriter.Close();
+                privateKeyWriter.Dispose();
+                isPrivateKeySaved = true;
+            }
+            else
+            {
+                isPrivateKeySaved = false;
+            }
+
+            bool isPublicKeySaved = false;
+            saveFileDialog1.Title = "Save Public Key";
+            saveFileDialog1.FileName = "PublicKey.puk";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                publicKeyWriter = new StreamWriter(saveFileDialog1.FileName);
+                publicKeyWriter.Write(xmlPublicKey);
+                publicKeyWriter.Close();
+                publicKeyWriter.Dispose();
+                isPublicKeySaved = true;
+            }
+            else
+            {
+                isPublicKeySaved = false;
+            }
+
+            String msg = "";
+            if (isPrivateKeySaved)
+            {
+                msg += "Your Private Key has been saved!";
+            }
+            else
+            {
+                msg += "Your Private Key has not been saved!";
+            }
+            if (isPublicKeySaved)
+            {
+                msg += "\n" + "Your Public Key has been saved!";
+            }
+            else
+            {
+                msg += "\n" + "Your Public Key has not been saved!";
+            }
+            MessageBox.Show(msg);
+        }
+
+        private void btnSourceBrowse_Click(object sender, EventArgs e)
+        {
+            String sourcePath = "";
+            openFileDialog1.Title = "Select file";
+            openFileDialog1.Filter = "All Files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                sourcePath = openFileDialog1.FileName;
+                txbSourcePath.Text = sourcePath;
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            // Select target CSP
+            CspParameters cspParams = new CspParameters();
+            cspParams.ProviderType = 1;
+            RSACryptoServiceProvider rsaProvider = new RSACryptoServiceProvider(cspParams);
+
+            // Import public key
+            String xmlPublicKey = GetKeyFromFile(txbPulicKeyFilePath.Text);
+            rsaProvider.FromXmlString(xmlPublicKey);
+
+            // Get bytes data to encrypt
+            byte[] toEncrypt = GetDataFromFileInByte(txbSourcePath.Text);
+
+            // Encryption
+            byte[] encrypted = rsaProvider.Encrypt(toEncrypt, true);
+
+            // Save encrypted data
+            String outputFileName = txbSourcePath.Text + ".rsa";
+            bool result = SaveOutput(outputFileName, encrypted);
+
+            if (result)
+            {
+                MessageBox.Show("Encryption process completed successful!");
+            }
+            else
+            {
+                MessageBox.Show("Encryption process fails!");
+            }
+        }
+
+        private bool SaveOutput(String outputFileName, byte[] encrypted)
+        {
+            try
+            {
+                FileStream fs = File.Open(outputFileName, FileMode.OpenOrCreate, FileAccess.Write);
+                BinaryWriter br = new BinaryWriter(fs);
+                br.Write(encrypted);
+
+                br.Close();
+                fs.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        private byte[] GetDataFromFileInByte(string sourcePath)
+        {
+            StreamReader reader = new StreamReader(sourcePath);
+            String strData = reader.ReadToEnd();
+            byte[] bytesData = Encoding.Unicode.GetBytes(strData);
+            reader.Close();
+            return bytesData;
+        }
+
+        private string GetKeyFromFile(string xmlKeyFileName)
+        {
+            StreamReader reader = new StreamReader(xmlKeyFileName);
+            String xmlPublicKey = "";
+
+            xmlPublicKey = reader.ReadToEnd();
+
+
+            return xmlPublicKey;
+
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+
+            // Select target CSP
+            CspParameters cspParams = new CspParameters();
+            cspParams.ProviderType = 1;
+            RSACryptoServiceProvider rsaProvider = new RSACryptoServiceProvider(cspParams);
+
+            // Import private key
+            String xmlPrivateKey = GetKeyFromFile(txbPrivateKeyFilePath.Text);
+            rsaProvider.FromXmlString(xmlPrivateKey);
+
+            // Get bytes data to encrypt
+            FileStream fs = File.OpenRead(txbSourcePath.Text);
+            byte[] encrypted = new byte[fs.Length];
+            fs.Read(encrypted, 0, (int)fs.Length);
+
+
+
+            // Decryption
+            byte[] decrypted = rsaProvider.Decrypt(encrypted, true);
+
+            // Save decrypted data
+
+            String outputFileName = "";
+            string[] strTemp = txbSourcePath.Text.Split('\\');
+            for (int i = 0; i < strTemp.Length - 1; i++)
+            {
+                outputFileName += strTemp[i] + "\\";
+            }
+            outputFileName += Path.GetFileNameWithoutExtension(txbSourcePath.Text);
+
+            bool result = SaveOutput(outputFileName, decrypted);
+
+            if (result)
+            {
+                MessageBox.Show("Encryption process completed successful!");
+            }
+            else
+            {
+                MessageBox.Show("Encryption process fails!");
+            }
+        }
+
+        private void btnPublicKeyBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select public key file.";
+            openFileDialog1.Filter = "PUK Files (*.puk)|*.puk";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txbPulicKeyFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnPrivateKeyBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select private key file.";
+            openFileDialog1.Filter = "PRK Files (*.prk)|*.prk";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txbPrivateKeyFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void txbSourcePath_TextChanged(object sender, EventArgs e)
+        {
+            String fileName = txbSourcePath.Text;
+            if (fileName != "")
+            {
+                String ext = Path.GetExtension(fileName);
+                if (ext.Equals(".rsa"))
+                {
+                    btnPrivateKeyBrowse.Enabled = true;
+                    btnPublicKeyBrowse.Enabled = false;
+                    txbPulicKeyFilePath.Text = "";
+
+                    btnDecrypt.Enabled = true;
+                    btnEncrypt.Enabled = false;
+                }
+                else
+                {
+                    btnPrivateKeyBrowse.Enabled = false;
+                    btnPublicKeyBrowse.Enabled = true;
+                    txbPrivateKeyFilePath.Text = "";
+
+                    btnEncrypt.Enabled = true;
+                    btnDecrypt.Enabled = false;
+                }
+            }
+            else
+            {
+                btnPrivateKeyBrowse.Enabled = false;
+                btnPublicKeyBrowse.Enabled = false;
+            }
+        }
+        
+    }
+}
